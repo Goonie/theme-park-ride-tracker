65dd71aab313187f9f9e8fc8fb8809071b52cd73
Goonie <github@goonie.nl>
2025-12-06 15:53:48 +0100

    Added useUserSession composable to get current user

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,28 @@
+import { ref } from 'vue';
+
 import { createClient } from '@supabase/supabase-js';
+
+import type { User } from '@supabase/supabase-js';
 
 const url = import.meta.env.VITE__SUPABASE__URL;
 const key = import.meta.env.VITE__SUPABASE__KEY;
 
 const supabase = createClient(url, key);
 
-export { supabase };
+/**
+ * Returns the current user.
+ *
+ * @returns A Supabase User object if signed in
+ * @returns Undefined if not signed in
+ */
+function useUserSession() {
+  const user = ref<User | undefined>();
+
+  supabase.auth.onAuthStateChange((_, session) => {
+    user.value = session?.user;
+  });
+
+  return { user };
+}
+
+export { supabase, useUserSession };
